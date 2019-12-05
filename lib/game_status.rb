224929ea4fa973ae494fdf3ab83f68efd393d4d1
Valerie Foster <fosterv2@outlook.com>
2019-12-05 22:20:24 +0000

    Automatically backed up by Learn

--- lib/game_status.rb
+++ lib/game_status.rb
@@ -15,12 +15,17 @@ WIN_COMBINATIONS = [
   [2,4,6]
 ]
 
+<<<<<<< HEAD
 def won?(board)
+=======
+def win?(board)
+>>>>>>> 9660615a800210c3d7a330dc9e139cfbc2e01304
   WIN_COMBINATIONS.each do |win_combination|
 #  for each win_combination in WIN_COMBINATIONS
     position_1 = board[win_combination[0]]
     position_2 = board[win_combination[1]]
     position_3 = board[win_combination[2]]
+<<<<<<< HEAD
     if position_taken?(board, win_combination[0])
       if position_1 == position_2 && position_2 == position_3
         return win_combination # return the win_combination indexes that won.
@@ -28,6 +33,16 @@ def won?(board)
     end
   end
   false
+=======
+    if position_taken(board, position_1)
+      if position_1 == position_2 && position_2 == position_3
+        return win_combination # return the win_combination indexes that won.
+      else
+        false
+      end
+    end
+  end
+>>>>>>> 9660615a800210c3d7a330dc9e139cfbc2e01304
 end
 
 def full?(board)
@@ -48,15 +63,25 @@ def draw?(board)
 #      true
 #    end
   if full?(board)
+<<<<<<< HEAD
     if !won?(board)
       return true
     end
   end
+=======
+    if !win?(board)
+      return trueend
+    end
+>>>>>>> 9660615a800210c3d7a330dc9e139cfbc2e01304
   false
 end
 
 def over?(board)
+<<<<<<< HEAD
   if won?(board).class == Array
+=======
+  if win?(board)
+>>>>>>> 9660615a800210c3d7a330dc9e139cfbc2e01304
     true
   elsif draw?(board)
     true
@@ -66,10 +91,17 @@ def over?(board)
 end
 
 def winner(board)
+<<<<<<< HEAD
   if !won?(board)
     nil
   else
     pos = won?(board)[0]
+=======
+  if !win?(board)
+    nil
+  else
+    pos = win?(board)[0]
+>>>>>>> 9660615a800210c3d7a330dc9e139cfbc2e01304
     board[pos]
   end
 end
